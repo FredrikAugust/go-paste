81f34376562764bd389228ec7d42fca014f3aaeb
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2017-02-14 20:59:52 +0100

    remove pastes from vcs, add license, get the paste back

--- src/main.go
+++ src/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/gorilla/mux"
 	"hash/fnv"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
+	"net/http"
 	"os"
 )
 
+// Max file size
 const MAX_LENGTH = 3 << 20 // 3 MiB
 
 func create() {
@@ -30,7 +34,7 @@ func create() {
 
 		// Respond
 		go func(c net.Conn) {
-			log.Print("Responding to incoming req from ", c.RemoteAddr())
+			log.Print("Responding to incoming req from: " + c.RemoteAddr().String())
 
 			buf := make([]byte, MAX_LENGTH)
 
@@ -47,6 +51,7 @@ func saveFileAndRespond(buf *[]byte, c net.Conn) {
 	h := fnv.New32a()
 	h.Write(*buf)
 
+	// Cheeky-breeky uint32->string
 	sum32String := fmt.Sprint(h.Sum32())
 
 	log.Print("Saving new paste with name: ", sum32String)
@@ -60,11 +65,55 @@ func saveFileAndRespond(buf *[]byte, c net.Conn) {
 
 	defer f.Close()
 
+	// Remove the null values from the file
 	io.WriteString(f, string(bytes.Trim(*buf, "\x00")))
 
-	io.WriteString(c, sum32String)
+	// Send back the newly created hash (name of the paste/file)
+	io.WriteString(c, "/"+sum32String)
+}
+
+// This will deal with the request to retrieve a paste
+func retrieveHandler(res http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+
+	if _, err := os.Stat("../pastes/" + vars["id"]); err != nil {
+		res.WriteHeader(http.StatusNoContent)
+		fmt.Fprint(res, "No such paste")
+		return
+	}
+
+	f, err := ioutil.ReadFile("../pastes/" + vars["id"])
+
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(res, "Error encountered while trying to access paste")
+		log.Fatal(err)
+		return
+	}
+
+	log.Print("Found file: ", vars["id"], " and now sending to user")
+
+	res.WriteHeader(http.StatusOK)
+
+	res.Write(f)
+}
+
+func retrieve() {
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Retrieve paste
+	router.HandleFunc("/{id:[1-9]+}", retrieveHandler).Methods("GET")
+
+	// Homepage
+	router.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		log.Print("Serving index.html")
+		http.ServeFile(res, req, "./index.html")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func main() {
+	go retrieve()
 	create()
 }
